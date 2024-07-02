320edc37f8a5f1a1e6fdee0c025955431afd7ee3
Administrator <admin@admins-mbp.home>
2024-07-02 09:41:54 -0400

    compressed version of boiler plate

--- backend/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\HomeController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-
-Route::post('upload-image', [HomeController::class, 'imageUpload']);
-Route::get('hello', [HomeController::class, 'hello']);
